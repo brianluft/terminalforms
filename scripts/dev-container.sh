857eb6388d1d1f4c16b0dffe95ac2c706998e521
Brian Luft <brian@electroly.com>
2026-01-31 16:03:17 -0800

    Use Claude Code reference devcontainer

--- scripts/dev-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-source "$( dirname "${BASH_SOURCE[0]}" )/env.sh"
-
-cd $ROOT_DIR/scripts
-docker build -t terminalforms-dev .
-
-docker run -it --rm -v "$ROOT_DIR":/repo terminalforms-dev
